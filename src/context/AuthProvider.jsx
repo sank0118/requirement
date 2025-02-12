d63a28fc7f9258181a4b53ac34d79a844103da83
sank0118 <tks9901@naver.com>
2025-02-12 09:04:30 +0900

    init

---
+++ src/context/AuthProvider.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import { useState, useEffect } from "react";
+import { authContext } from "./useAuth";
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    console.log({ user });
+  }, [user]);
+
+  return (
+    <authContext.Provider value={{ user, setUser }}>
+      {children}
+    </authContext.Provider>
+  );
+};
+
+AuthProvider.propTypes = {
+  children: PropTypes.element,
+};
---
+++ src/hooks/UseRandomId.js
@@ -0,0 +1,7 @@
+const useRandomId = () => {
+  let randomId = 0;
+  randomId = Math.floor(Math.random() * 100000000000000);
+  return randomId.toString();
+};
+
+export default useRandomId;
